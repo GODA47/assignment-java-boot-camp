d466c501d8be30dd1d07dece08d4cc388248afc1
GODA47 <87638507+GODA47@users.noreply.github.com>
2022-02-23 22:57:09 +0900

    #0 initial project upload

---
+++ Shooopie/src/main/java/com/example/Shooopie/ShooopieApplication.java
@@ -0,0 +1,14 @@
+package com.example.Shooopie;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShooopieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShooopieApplication.class, args);
+	}
+
+}
